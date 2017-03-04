9fc5bc8adef445f24648592645a71894755d68ff
jsheerin66 <user@users-MacBook-Pro-2.local>
2017-03-04 14:31:13 -0500

    commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,40 @@
-//var options = [ "a","b"."c","d","e","f","g","h","i","j","k","l","m", "n","o","p","q","r","s","q","u","v","w","x","y","z" ];
+var options = [ "a","b","c","d","e","f","g","h","i","j","k","l","m", "n","o","p","q","r","s","q","u","v","w","x","y","z" ];
+var wins= 0;
+var losses= 0;
+var guessesLeft= 9;
+var guessesSoFar= 0;
 
-var options = ["a","b","c"];
+var computerGuess = options[Math.floor(Math.random()*options.length)];
 
 document.onkeyup = function(event) {
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-  console.log(userGuess);
+  console.log("userGuess: " + userGuess);
+  console.log("computerGuess: " + computerGuess);
 
-  var guessesSoFar = document.getElementById('guessesSoFar');
-  guessesSoFar.innerHTML = userGuess;
-
-  var computerGuess = options[Math.floor(Math.random()*options.length)];
-
-  console.log(computerGuess)
 // why does it not alert "you are not psychic yet when userGuess is a,b,c?
-  if (userGuess == 'a' || userGuess == 'b' || userGuess == 'c')
-  {
-      if ((userGuess == computerGuess)){
-      alert("You are PSYCHIC!");
-      	}
-  }
-  else {
-      alert("Not psychic yet, try again!");
+	// one general line to cover all letters of alphabet
+
+  if ((userGuess == computerGuess)){
+      console.log("You are PSYCHIC!");
+  } else {
+      console.log("Not psychic yet");
   }
 
-	var html = "<h1>The Psychic Game</h1>";
+	if ((userGuess != computerGuess)){
+			guessesLeft --;
+			console.log("guessesLeft: " + guessesLeft);
+			guessesSoFar = userGuess;
+			console.log("guessesSoFar: " + guessesSoFar);
 
-	document.oneKeyUp.querySelector('#game').innerHTML= html;
+	}
+	var html = "<h1> The Psychic Game </h1>" +
+						"<p> Am I guessing... a || b, press a key to c</p>" +
+			    	"<p> Wins: </p>" +
+			    	"<p> Losses: </p>" +
+			    	"<p> Guesses Left: " + guessesLeft + " </p>" +
+						"<p> Your Guesses so far: " + guessesSoFar + "  </p>";
+document.querySelector('#game').innerHTML = html;
 
+	// understand what the + variable + means
 }
